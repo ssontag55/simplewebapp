ef04e782e066ecd60620c65dbd00115460d3ca77
ssontag55 <sontag.stephen@gmail.com>
2017-09-01 14:36:38 -0400

    configure up incident json tags - limited

--- app/app.js
+++ app/app.js
@@ -1,10 +1,9 @@
 function startupMap(){
     me = this;
 
-    this.datalist = [];
-    me.resultLyr = [];
-    me.resultInterpolateLyr = [];
-    me.resultHeatmapLyr = [];
+    me.responseLayer = [];
+    me.weatherLayer = [];
+    me.parcelLayer = [];
 
     me.map = L.map('viewDiv').setView([30.458,-91.1811], 15);
     me.layer = L.esri.basemapLayer('Gray').addTo(map);
@@ -17,13 +16,16 @@ function startupMap(){
     getLayerList();
 
     getIncidentConfig();
+
+    $("#responseToggle").click(me.updateresponseLayer);
+    $("#weatherToggle").click(me.updateweatherLayer);
+    $("#parcelLayerToggle").click(me.updateparcelLayer);
 }
 
 function getLayerList(){
 
-
   //limitting features -- super slow service
-  var parcels = L.esri.featureLayer({
+  me.parcelLayer = L.esri.featureLayer({
     url: "http://gis.richmondgov.com/ArcGIS/rest/services/WebMercator/Parcels/MapServer/2",
     onEachFeature: function (feature, layer) {
       var popupContent = L.Util.template('<p><b>Parcel SQ Feet</b> - {LandSqFt}<br><br><b>Total Value</b> - $' + '{TotalValue}' + '<br><b>Property Class</b> - {PropertyClass} <br><b>Owner Info</b> - {OwnerName}<br><b>Address</b> - {MailAddress}<br><b>City</b> - {MailCity}<br><b>Land Use</b> - {LandUse}</p>',feature.properties);
@@ -34,25 +36,73 @@ function getLayerList(){
           return {color: '#bada55', weight: 2 };
       }
   });
-  parcels.addTo(me.map);
+  //me.parcelLayer.addTo(me.map);
 
-  parcels.on('mouseover', function (e) {
+  me.parcelLayer.on('mouseover', function (e) {
       e.layer.openPopup();
   });
-  parcels.on('mouseout', function (e) {
+  me.parcelLayer.on('mouseout', function (e) {
       e.layer.closePopup();
+  }); 
+
+  //limited by date -- only showing current time
+  me.weatherLayer = L.esri.dynamicMapLayer({
+    url: "https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer",
+    opacity: .2
   });
-  
+
+  me.weatherLayer.addTo(me.map);
 }
 
 function getIncidentConfig(){
 
   $.getJSON( "data/F01705150050.json", function( data ) {
       //me.map.flyTo([data.address.latitude,data.address.longitude],13);
-      me.map.setView([data.address.latitude,data.address.longitude],13);      
-  });
+      me.map.setView([data.address.latitude,data.address.longitude],14);      
 
-  $('#loadinggf').hide();
+
+      var alertIcon = L.icon({
+        iconUrl: 'assets/alert.gif',
+      });
+      L.marker([data.address.latitude,data.address.longitude], {icon: alertIcon}).addTo(me.map).bindPopup(data.description.subtype);
+
+      $('#incidentID').text("Incident Number: " + data.description.incident_number);
+      $('#incidentDescription').text(data.description.comments);
+      $('#incidentFireDept').text("Fire Dept: "+ data.fire_department.name);
+      $('#incidentresponseTime').text("Reponse Time: "+ Number(data.description.extended_data.response_time)/60 + ' minutes');
+      $('#incidentStart').text("Start: "+ data.description.event_opened);
+      $('#incidentEnd').text("End: "+ data.description.event_closed);       
+
+      $('#loadinggf').hide();
+  });
+}
+
+function updateresponseLayer(){
+
+  if($('#responseToggle').prop('checked') == true){
+      map.addLayer(me.responseLayer);
+  }
+  else{
+      map.removeLayer(me.responseLayer);
+  }
+}
+
+function updateweatherLayer(){
+  if($('#weatherToggle').prop('checked') == true){
+      map.addLayer(me.weatherLayer);
+  }
+  else{
+      map.removeLayer(me.weatherLayer);
+  }  
+}
+
+function updateparcelLayer(){
+  if($('#parcelLayerToggle').prop('checked') == true){
+      map.addLayer(me.parcelLayer);
+  }
+  else{
+      map.removeLayer(me.parcelLayer);
+  }  
 }
 
 /* Set Basemap */
